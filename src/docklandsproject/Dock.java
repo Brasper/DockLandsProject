9f1614a2e17858a6d8de1aa8fd124d2c2854405c
paul1 <paul1@DESKTOP-CDOVMIK.Home>
2026-03-14 18:39:42 +0000

    Commit 2 Dock and MaintenanceRequest Classes added

--- src/docklandsproject/Dock.java
+++ src/docklandsproject/Dock.java
@@ -9,5 +9,66 @@ package docklandsproject;
  * @author paul1
  */
 public class Dock {
+    //Fields
+    private int dockId;
+    private String location;
+    private int bikesAvailable;
+    private int scootersAvailable;
+    private String status;
+
+    public Dock() {
+    }
+
+    public Dock(int dockId, String location, int bikesAvailable, int scootersAvailable, String status) {
+        this.dockId = dockId;
+        this.location = location;
+        this.bikesAvailable = bikesAvailable;
+        this.scootersAvailable = scootersAvailable;
+        this.status = status;
+    }
+
+    public int getDockId() {
+        return dockId;
+    }
+
+    public void setDockId(int dockId) {
+        this.dockId = dockId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getBikesAvailable() {
+        return bikesAvailable;
+    }
+
+    public void setBikesAvailable(int bikesAvailable) {
+        this.bikesAvailable = bikesAvailable;
+    }
+
+    public int getScootersAvailable() {
+        return scootersAvailable;
+    }
+
+    public void setScootersAvailable(int scootersAvailable) {
+        this.scootersAvailable = scootersAvailable;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
     
+    public String printDetails(){
+        return "Dock ID:" + dockId + "Location:" + location + "Bikes:" + bikesAvailable + "Scooters:" + scootersAvailable + "Status:" + status;
+        
+    }
 }
--- src/docklandsproject/MaintenanceRequest.java
+++ src/docklandsproject/MaintenanceRequest.java
@@ -4,10 +4,68 @@
  */
 package docklandsproject;
 
+
+    
+
 /**
  *
  * @author paul1
  */
-public class MaintenanceRequest {
+public class MaintenanceRequest implements Comparable<MaintenanceRequest>{
+    //Fields
+    private int requestId;
+    private int dockId;
+    private String issueDescription;
+    private int priorityLevel;
+
+    public MaintenanceRequest() {
+    }
+
+    public MaintenanceRequest(int requestId, int dockId, String issueDescription, int priorityLevel) {
+        this.requestId = requestId;
+        this.dockId = dockId;
+        this.issueDescription = issueDescription;
+        this.priorityLevel = priorityLevel;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getDockId() {
+        return dockId;
+    }
+
+    public void setDockId(int dockId) {
+        this.dockId = dockId;
+    }
+
+    public String getIssueDescription() {
+        return issueDescription;
+    }
+
+    public void setIssueDescription(String issueDescription) {
+        this.issueDescription = issueDescription;
+    }
+
+    public int getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(int priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
     
+    @Override
+    public int compareTo(MaintenanceRequest other){
+        return Integer.compare(this.priorityLevel, other.priorityLevel);
+    }
+    
+    public String printDetails(){
+        return "Request ID:" + requestId + "Dock ID:" + dockId + "Issue:" + issueDescription + "Priority:" + priorityLevel;
+    }
 }
