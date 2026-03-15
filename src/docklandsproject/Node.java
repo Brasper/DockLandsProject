76d85fba4f88c3690d4b9b1fc05b2db7f544526b
paul1 <paul1@DESKTOP-CDOVMIK.Home>
2026-03-15 16:03:00 +0000

    Commit 5 Node class done

--- src/docklandsproject/Node.java
+++ src/docklandsproject/Node.java
@@ -9,5 +9,29 @@ package docklandsproject;
  * @author paul1
  */
 public class Node {
+    private Dock dock;
+    private Node next;
+
+    public Node(Dock dock, Node next) {
+        this.dock = dock;
+        this.next = next;
+    }
+
+    public Dock getDock() {
+        return dock;
+    }
+
+    public void setDock(Dock dock) {
+        this.dock = dock;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
     
 }
