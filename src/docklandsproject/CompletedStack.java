2e68169d42ad949d725b8d1b160707ad694623d2
paul1 <paul1@DESKTOP-CDOVMIK.Home>
2026-03-15 18:56:18 +0000

    Commit 9 Completed Stack done

--- src/docklandsproject/CompletedStack.java
+++ src/docklandsproject/CompletedStack.java
@@ -4,10 +4,48 @@
  */
 package docklandsproject;
 
+import java.util.Stack;
+
 /**
  *
  * @author paul1
  */
 public class CompletedStack {
+    
+    private Stack<MaintenanceRequest> stack;
+
+    public CompletedStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(MaintenanceRequest r) {
+        stack.push(r);
+    }
+    
+    public MaintenanceRequest pop(){
+        if(! stack.isEmpty()){
+            return stack.pop();
+        }
+        
+        return null;
+    }
+    
+    public void viewHistory(){
+        if (stack.isEmpty()){
+            System.out.println("No Completed requests done yet");
+            return;
+        }
+        for(MaintenanceRequest r : stack){
+            System.out.println(r.printDetails());
+        }
+    }
+    
+    public boolean isEmpty(){
+        if (stack.isEmpty()){
+        return true;
+    } else {
+            return false;
+            }
+    }
     
 }
