b7ef6d2b37a968bbe88d5e9fdb62b83607bf93c0
paul1 <paul1@DESKTOP-CDOVMIK.Home>
2026-03-17 01:08:20 +0000

    Commit 13 added comments to the dock and completed stack classes

--- src/docklandsproject/CompletedStack.java
+++ src/docklandsproject/CompletedStack.java
@@ -12,34 +12,38 @@ import java.util.Stack;
  */
 public class CompletedStack {
     
+    //private fields
     private Stack<MaintenanceRequest> stack;
-
+    //constructor
     public CompletedStack() {
         stack = new Stack<>();
     }
-    
+    //pushes a completed request onto the top of the stack gets called when maintenacance request is processed
     public void push(MaintenanceRequest r) {
         stack.push(r);
     }
-    
+    //removes and returns the top from the stack and returns if null is empty
     public MaintenanceRequest pop(){
+        // checks stack is empty before popping
         if(! stack.isEmpty()){
             return stack.pop();
         }
-        
+       
         return null;
     }
-    
+    // it displays all completed requests in the stack
     public void viewHistory(){
+        //checks if stack is empty
         if (stack.isEmpty()){
             System.out.println("No Completed requests done yet");
             return;
         }
+        //loops thouhg stack and prints each request
         for(MaintenanceRequest r : stack){
             System.out.println(r.printDetails());
         }
     }
-    
+    //returns true if the stack is empty and false if it is not
     public boolean isEmpty(){
         if (stack.isEmpty()){
         return true;
--- src/docklandsproject/Dock.java
+++ src/docklandsproject/Dock.java
@@ -9,16 +9,16 @@ package docklandsproject;
  * @author paul1
  */
 public class Dock {
-    //Fields
+    // private Fields
     private int dockId;
     private String location;
     private int bikesAvailable;
     private int scootersAvailable;
     private String status;
-
+    // default constructor
     public Dock() {
     }
-
+    //full constructor sets all dock the docks fields
     public Dock(int dockId, String location, int bikesAvailable, int scootersAvailable, String status) {
         this.dockId = dockId;
         this.location = location;
@@ -26,47 +26,48 @@ public class Dock {
         this.scootersAvailable = scootersAvailable;
         this.status = status;
     }
-
+    //returns dockid
     public int getDockId() {
         return dockId;
     }
-
+    // sets dockid
     public void setDockId(int dockId) {
         this.dockId = dockId;
     }
-
+    // returns location
     public String getLocation() {
         return location;
     }
-
+    //sets location
     public void setLocation(String location) {
         this.location = location;
     }
-
+    // returnsm bikesavailable
     public int getBikesAvailable() {
         return bikesAvailable;
     }
-
+    // sets bikesavailable
     public void setBikesAvailable(int bikesAvailable) {
         this.bikesAvailable = bikesAvailable;
     }
-
+    // returns availablescooters
     public int getScootersAvailable() {
         return scootersAvailable;
     }
-
+    // gets scootersavailable
     public void setScootersAvailable(int scootersAvailable) {
         this.scootersAvailable = scootersAvailable;
     }
-
+    //returns status
     public String getStatus() {
         return status;
     }
-
+    //sets status
     public void setStatus(String status) {
         this.status = status;
     }
-    
+    // returns all dock details as a String
+    // it is used to display dokc infomration in the GUI
     public String printDetails(){
         return "Dock ID:" + dockId + "| Location:" + location + " | Bikes:" + bikesAvailable + " | Scooters:" + scootersAvailable + " |Status:" + status;
         
