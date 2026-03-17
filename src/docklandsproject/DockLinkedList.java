feab68f5b2d6b6ab1cd7cb2e1e99dfc9177f4fb1
paul1 <paul1@DESKTOP-CDOVMIK.Home>
2026-03-17 01:23:06 +0000

    Commit 14 added comments to docklinkedlist and the dockoperations

--- src/docklandsproject/DockLinkedList.java
+++ src/docklandsproject/DockLinkedList.java
@@ -10,28 +10,34 @@ package docklandsproject;
  */
 public class DockLinkedList implements DockOperations {
     
+    //head points to the first node in the list
     private Node head;
-    
+    //constructor
     public DockLinkedList(){
         head = null;
     }
-    
+    //adds a new dock to the end of the linked lsit
     @Override
     public void addDock(Dock d) {
+        //create new node witht he dock object
         Node newNode = new Node(d);
+        // if the list is empty new node beocmes the head
         if (head == null) {
             head = newNode;
         } else {
+            //travel to the end of the list
             Node current = head;
             while (current.getNext() != null) {
                 current = current.getNext();
             }
+            // adds a new node at the end 
             current.setNext(newNode);
         }
     }
-
+    // removes a dock from the list by its id
     @Override 
     public void removeDock(int dockId){
+        // returns if list is empty
         if(head == null){
             return;
         }
@@ -39,6 +45,7 @@ public class DockLinkedList implements DockOperations {
             head = head.getNext();
             return; 
         }
+        //travels list to find the node to remove
         Node current = head;
         while(current.getNext()!=null){
             if (current.getNext().getDock().getDockId() == dockId){
@@ -48,23 +55,28 @@ public class DockLinkedList implements DockOperations {
             current = current.getNext();
         }
     }
-    
+    //Finds and returns one dock by its ID
     @Override
     public Dock findDock(int dockId) {
+        //start at the head and traverse the list
         Node current = head;
         while (current !=null){
+            // return dock if ID matches
             if (current.getDock().getDockId()== dockId){
                 return current.getDock();
                 }
             current = current.getNext();
         }
+        // returns null if dock is not found
         return null;
     }
-    
+    // updates existing dock in the list
     @Override
     public void updateDock(Dock d ){
+        // starts at the head and traverse the list
         Node current = head;
         while (current !=null) {
+            // find dock with the matching id and update it 
             if (current.getDock().getDockId()== d.getDockId()){
                 current.setDock(d);
                 return;
@@ -72,12 +84,14 @@ public class DockLinkedList implements DockOperations {
             current = current.getNext();
         }
     }
+    // displays all docks in the list
     @Override
     public void displayDocks(){
         if(head == null) {
             System.out.println("No docks are in the system");
             return;
         }
+        //traverse the list and print each dock
         Node current = head;
         while (current !=null){
             System.out.println(current.getDock().printDetails());
--- src/docklandsproject/DockOperations.java
+++ src/docklandsproject/DockOperations.java
@@ -10,14 +10,15 @@ package docklandsproject;
  */
 public interface DockOperations {
     
+    // adds a new dock to the linked list
     void addDock(Dock d);
-    
+    // removes a dock in the linked list by its ID
     void removeDock(int dockId);
-    
+    // finds and returns dock by ID
     Dock findDock(int dockId);
-    
+    // Updates an exsisting dock in the linked list
     void updateDock( Dock d );
-    
+    // displays all docks in the linked lists
     void displayDocks();
     
 }
